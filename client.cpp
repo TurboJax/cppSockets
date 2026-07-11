75b61ceea3ced09756fdf2f3bbeb85df65062193
TurboJax <turbojax07@gmail.com>
2026-07-11 00:35:29 -0400

    Letting users enter their own input

--- client.cpp
+++ client.cpp
@@ -39,8 +39,11 @@ int main(int argc, char** argv) {
         std::cout << "[+] Successfully connected to the server!\n";
     }
 
+    // Read line of data from user
+    std::string msg;
+    std::getline(std::cin, msg);
+
     // Send data to server
-    std::string msg = "Hello, server!";
     int sendMsg = send(clientSock, msg.c_str(), msg.length(), 0);
     if(sendMsg == -1){
         std::cout << "[!] Unable to send message to the server, please try again.\n";
